197cab1f258f414055c1b2df904e46cf21826c69
Eskil Høstad <eskil.hostad@huddly.com>
2022-01-21 08:45:23 +0100

    feat: Support new autozoom mode 'Plaza' (aka 'Gallery View') on IQ

---
+++ src/enums/AutozoomModes.ts
@@ -0,0 +1,10 @@
+/**
+ * Lower case naming scheme to match target reported values.
+ */
+enum AutozoomModes {
+  NORMAL = 'normal',
+  PLAZA = 'plaza',
+  PLAZA_DUPLICATE = 'plaza_duplicate',
+}
+
+export default AutozoomModes;
