40a4bfdcaec909300665d4ffdf6f454a95de6ac6
Patrik <patrik@huddly.com>
2022-09-27 07:09:08 +0200

    feat: added speaker_framing enum

--- src/enums/AutozoomModes.ts
+++ src/enums/AutozoomModes.ts
@@ -1,10 +1,12 @@
 /**
- * Lower case naming scheme to match target reported values.
+ * Lower case naming scheme to match target reported values for boxfish.
  */
 enum AutozoomModes {
   NORMAL = 'normal',
   PLAZA = 'plaza',
   PLAZA_DUPLICATE = 'plaza_duplicate',
+  SPEAKER_FRAMING = 'speaker_framing',
+  OFF = 'off',
 }
 
 export default AutozoomModes;
