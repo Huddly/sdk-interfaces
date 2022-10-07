0314112b10f700e1b2f1f0e2757123e8ff1b0df0
Patrik <patrik@huddly.com>
2022-10-07 13:28:31 +0200

    feat(IAutozoomControl): added function for getting supported framing modes

--- src/interfaces/IAutozoomControl.ts
+++ src/interfaces/IAutozoomControl.ts
@@ -3,6 +3,13 @@ import ICnnControl from './ICnnControl';
 
 export default interface IAutozoomControl extends ICnnControl {
   /**
+   * Gets the supported framing modes for the camera
+   *
+   * @return {Promise<FramingModes[]>} Returns a promise that resolves an array of supported modes
+   * @memberof IAutozoomControl
+   */
+  getSupportedFramingModes(): Promise<FramingModes[]>;
+  /**
    * Sets autozoom mode on camera
    *
    * @param {FramingModes} framingMode What framing mode to set
@@ -11,5 +18,5 @@ export default interface IAutozoomControl extends ICnnControl {
    * succesfully. Reject with an error otherwise.
    * @memberof IAutozoomControl
    */
-  setFramingModes(framingMode: FramingModes): Promise<void>;
+  setFramingMode(framingMode: FramingModes): Promise<void>;
 }
