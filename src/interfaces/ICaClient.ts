f9fd5ac1cd15d2c1f05ee1b01376db7d1656e1b6
Patrik <patrik@huddly.com>
2023-03-02 09:12:48 +0100

    added some doc strings and option type

--- src/interfaces/ICaClient.ts
+++ src/interfaces/ICaClient.ts
@@ -1,6 +1,26 @@
+/**
+ *
+ */
+export const Option = {
+  speakerframing: 'speakerframing',
+} as const;
+
+/**
+ * Mirrors the expected structure of the server response for each option
+ * certificate.
+ */
 export interface OptionCertificate {
+  /**
+   * Format of the certificate. Typically Pem
+   */
   format: string;
-  option: string;
+
+  /**
+   * Name of the option the certificate is meant for. An option
+   * can for instance be a feature such as speaker framing. Typically as a single
+   * string of combined words without any spaces.
+   */
+  option: typeof Option;
   data: string;
 }
 
@@ -17,6 +37,12 @@ export function isOptionCertificate(
   );
 }
 
+/**
+ * Client used for communicating with the Huddly Certificate Authorithy.
+ * Main use is to get option certificates for a given camera based on its
+ * serial number.
+ *
+ */
 export default interface ICaClient {
   getOptionCertificates(serialNumber: string): Promise<OptionCertificatesResult>;
 }
