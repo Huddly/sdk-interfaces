c656e20f9b71ec4f3db3fb8e7e46d1259518d3e3
Patrik <patrik@huddly.com>
2023-03-21 08:19:19 +0100

    feat: added pids for cameras connected to smartbase

--- src/enums/HuddlyHex.ts
+++ src/enums/HuddlyHex.ts
@@ -13,6 +13,9 @@ enum HuddlyHex {
   L1_PID = 0x4d3f64b, // Huddly L1/ACE => From onvif wsdd discovery response (decimal val: 81000011)
   S1_PID = 0x4d3f64e, // Huddly S1/SEE => From onvif wsdd discovery response (decimal val: 81000014)
   SMARTBASE_PID = 0xba01, // Huddly Smartbase
+  S1_SMARTBASE_PID = 0xa031, // Huddly S1/SEE when connected to smartbase
+  L1_SMARTBASE_PID = 0xa032, // Huddly L1/ACE when connected to smartbase
+  CREW_SMARTBASE_PID = 0xa033, // Huddly Crew when connected to smartbase
 }
 
 export default HuddlyHex;
