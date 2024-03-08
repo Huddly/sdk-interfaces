ef760a32689658b1d8ba03ac491edb741efc4197
Patrik <patrik@huddly.com>
2024-03-08 11:10:05 +0100

    use mode names from target in enums

--- src/enums/DirectorModes.ts.ts
+++ src/enums/DirectorModes.ts.ts
@@ -1,6 +1,9 @@
+/**
+ * Naming scheme to match strings used on target
+ */
 enum DirectorModes {
-  Speaker,
-  Collaboration,
+  Speaker = 'speaker-centric',
+  Collaboration = 'default',
 }
 
 export default DirectorModes;
