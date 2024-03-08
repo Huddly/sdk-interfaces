45cd3558a48d3f179476e7c23e19301fdecee135
Patrik <patrik@huddly.com>
2024-03-08 13:07:19 +0100

    as any for easy conversion

--- src/enums/DirectorModes.ts.ts
+++ src/enums/DirectorModes.ts.ts
@@ -2,8 +2,8 @@
  * Naming scheme to match strings used on target
  */
 enum DirectorModes {
-  Speaker = 'speaker-centric',
-  Collaboration = 'default',
+  Speaker = 'speaker-centric' as any,
+  Collaboration = 'default' as any,
 }
 
 export default DirectorModes;
